2d143ea0b5edbb2e5eebe7f5104f9d96526e89c2
Carson <a@Aui-MacBookAir.local>
2021-10-15 12:46:28 +0900

    2

--- main.go
+++ main.go
@@ -14,5 +14,7 @@ func main() {
 
 	for _, block := range chain.AllBlocks() {
 		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
 	}
 }
