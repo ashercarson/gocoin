e861bfa79cddf0afe4bec9309a62844f37001b74
Carson <a@Aui-MacBookAir.local>
2021-10-15 12:37:50 +0900

    1

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "./blockchain"
+import (
+	"fmt"
+
+	"github.com/ashercarson/gocoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
-	println(chain)
+	chain.AddBlock("Second Blcok")
+	chain.AddBlock("Third Blcok")
+	chain.AddBlock("Fourth Blcok")
+
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+	}
 }
